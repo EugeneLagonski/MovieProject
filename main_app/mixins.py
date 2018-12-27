7247b14a7681041daa25adfe1be95bab69509dd5
Eugene Lagonski <Lagonski.Eugene@tut.by>
2018-12-27 12:53:08 +0300

    Added logging to non-read endpoints

--- main_app/mixins.py
+++ main_app/mixins.py
@@ -1,4 +1,38 @@
 from django.db import models
+import logging
+from rest_framework import status
+from rest_framework.views import APIView
+from MovieProject.logging import setup_logging
+
+setup_logging()
+logger = logging.getLogger('views')
+
+
+class LoggingMixin(APIView):
+    # log only when one of the following methods is being performed
+    allowed_logging_methods = ('post', 'put', 'patch', 'delete')
+
+    def finalize_response(self, request, response, *args, **kwargs):
+        # regular finalize response
+        response = super().finalize_response(request, response, *args, **kwargs)
+        # do not log, if method not found
+        if request.method.lower() not in self.allowed_logging_methods:
+            return response
+        status_code = response.status_code
+        log_kwargs = {
+            'view': self.get_view_name(),
+            'action': self.action,
+            'method': request.method.lower(),
+            'status_code': status_code,
+            'request_path': request.path,
+        }
+        if status.is_server_error(status_code):
+            logger.error('error', extra=log_kwargs)
+        elif status.is_client_error(status_code):
+            logger.warning('warning', extra=log_kwargs)
+        else:
+            logger.info('info', extra=log_kwargs)
+        return response
 
 
 class Timestamps(models.Model):
--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,19 @@
 from rest_framework import viewsets
-
+from main_app.mixins import LoggingMixin
 from main_app import models
 from main_app import serializers
 
 
-class DirectorViewSet(viewsets.ModelViewSet):
+class DirectorViewSet(LoggingMixin, viewsets.ModelViewSet):
     queryset = models.Director.objects.all()
     serializer_class = serializers.DirectorSerializer
 
 
-class ActorViewSet(viewsets.ModelViewSet):
+class ActorViewSet(LoggingMixin, viewsets.ModelViewSet):
     queryset = models.Actor.objects.all()
     serializer_class = serializers.ActorSerializer
 
 
-class MovieViewSet(viewsets.ModelViewSet):
+class MovieViewSet(LoggingMixin, viewsets.ModelViewSet):
     queryset = models.Movie.objects.all()
     serializer_class = serializers.MovieSerializer
