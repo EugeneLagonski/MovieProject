942fa772e5ca058a3b019781e581c44e8729cbe9
Eugene Lagonski <Lagonski.Eugene@tut.by>
2019-02-04 10:55:14 +0300

    Code style fix

--- frontend/src/components/ActorDetail.js
+++ frontend/src/components/ActorDetail.js
@@ -4,13 +4,10 @@ import 'whatwg-fetch'
 import {Card, CardBody, CardSubtitle, CardText, CardTitle} from "reactstrap";
 
 export default class ActorDetail extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            id: props.match.params.actorId,
-            name: null
-        }
-    }
+    state = {
+        id: props.match.params.actorId,
+        name: null
+    };
 
     fetchData() {
         fetch('http://api.localhost/actors/' + this.state.id)
